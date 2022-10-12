8eeb07801be399a084f423c475d978ea66eb4c6d
Sergey Kuznetsov <mr.enline@gmail.com>
2022-10-12 18:30:02 +0300

    begin execute task

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -1,6 +1,4 @@
-
-const HeroesListItem = ({name, description, element}) => {
-
+const HeroesListItem = ({ id, name, description, element, deleteHero }) => {
     let elementClassName;
 
     switch (element) {
@@ -21,22 +19,22 @@ const HeroesListItem = ({name, description, element}) => {
     }
 
     return (
-        <li 
-            className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
-            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg" 
-                 className="img-fluid w-25 d-inline" 
-                 alt="unknown hero" 
-                 style={{'objectFit': 'cover'}}/>
+        <li className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
+            <img
+                src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg"
+                className="img-fluid w-25 d-inline"
+                alt="unknown hero"
+                style={{ objectFit: 'cover' }}
+            />
             <div className="card-body">
-                
                 <h3 className="card-title">{name}</h3>
                 <p className="card-text">{description}</p>
             </div>
             <span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
-                <button type="button" className="btn-close btn-close" aria-label="Close"></button>
+                <button type="button" className="btn-close btn-close" aria-label="Close" onClick={() => deleteHero(id)}></button>
             </span>
         </li>
-    )
-}
+    );
+};
 
 export default HeroesListItem;
